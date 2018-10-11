20bf4c0c3360d2f874491c64189fe76615d6efbb
Marina <marina.marlagkoutsou@pressbox.com.gr>
2018-10-11 10:25:28 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+// the paths
+
+var jsSources = ['scripts/*.js'],
+    sassSources = ['styles/*.scss'],
+    htmlSources = ['**/*.html'],
+    outputDir = 'assets';
+
+
+
+gulp.task('sass', function() {
+  gulp.src(sassSources)
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('assets'))
+  .pipe(connect.reload())
+});
+
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+  .pipe(uglify())
+  .pipe(concat('script.js'))
+  .pipe(gulp.dest(outputDir))
+  .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(sassSources, ['sass']);
+  gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  })
+});
+
+gulp.task('html', function() {
+  return gulp.src(htmlSources)
+  .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'js', 'sass', 'connect', 'watch']);
+
+
+// localhost:8080
